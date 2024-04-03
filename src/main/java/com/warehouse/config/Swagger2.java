c095542222ba3053d7309efe1e3751e32068d237
dings <1203823603@qq.com>
2024-04-04 01:09:55 +0800

    fix swagger

---
+++ src/main/java/com/warehouse/config/Swagger2.java
@@ -0,0 +1,53 @@
+package com.warehouse.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("spring-platform-interface")
+                //加载配置信息
+                .apiInfo(apiInfo()).globalOperationParameters(setHeaderToken()).select()
+                //加载swagger 扫描包
+                .apis(RequestHandlerSelectors.basePackage("com.warehouse.controller")).paths(PathSelectors.any()).build();
+    }
+
+
+    /**
+     * 获取swagger创建初始化信息
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("wms", "", "1203823603`@qq.com");
+        return new ApiInfoBuilder().title("swagger 测试文档").description("dev by ds").contact(contact).version("1.0.0").build();
+    }
+
+    private List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("token").description("token").modelRef(new ModelRef("string")).parameterType("header")
+                .defaultValue("abc")
+                .build();
+        pars.add(tokenPar.build());
+        return pars;
+    }
+}
